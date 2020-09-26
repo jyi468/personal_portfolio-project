3a88ec8bd349b09f3c70de248cd63821aa6dcddc
Josh <jyi468@gmail.com>
2020-09-26 13:55:08 -0400

    Add new model

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='portfolio/images/')  # you can upload these photos to a location
+    url = models.URLField(blank=True)  # can add blank as a property to anything
