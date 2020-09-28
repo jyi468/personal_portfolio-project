ec71fa8416fdea0052d8d9435769ea09e1bb59ec
Josh <jyi468@gmail.com>
2020-09-28 13:47:16 -0400

    Add home page. Add portfolio templating. Add image urls

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -10,12 +10,20 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
+    1. Import the include() function: f.rom django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from portfolio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
 ]
+
+# Django media images
+# static - Return a URL pattern for serving files in debug mode.
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Project
+
 
 # Create your views here.
+def home(request):
+    # Grab Project objects from the DB. From DB to View
+    projects = Project.objects.all()  # Grab all project objects from DB
+    return render(request, 'portfolio/home.html', {'projects': projects})
