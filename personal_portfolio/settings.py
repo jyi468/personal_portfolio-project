3499c507566d917b38b706b989b52ee8c93a96d3
Josh <jyi468@gmail.com>
2020-09-28 15:51:34 -0400

    Add static files

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -119,7 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/'  # root for static files
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'  # directory to store images, other media.
