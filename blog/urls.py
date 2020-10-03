38e7f084ae7ed3e2a66c166a5e76c934e2be3b11
Josh <jyi468@gmail.com>
2020-10-03 18:06:47 -0400

    Add views

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from . import views  # import blog's views
 
+app_name = 'blog'  # specify app name's detail. like blog:detail
+
 urlpatterns = [
-    path('', views.all_blogs, name='all_blogs')  # if forwarded, it has the other url added to it
+    path('', views.all_blogs, name='all_blogs'),  # if forwarded, it has the other url added to it
+    path('<int:blog_id>/', views.detail, name='detail')  # match int blog_id from address and pass detail in views
 ]
